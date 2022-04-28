4a64ca19517938ed913c209349e2ffffab10f24e
wandertaker <wandertaker@users.noreply.github.com>
2022-04-28 14:53:50 +0200

    Add smaragdschwert

--- src/main/java/net/fabricmc/example/SmaragdschwertMod.java
+++ src/main/java/net/fabricmc/example/SmaragdschwertMod.java
@@ -1,10 +1,17 @@
 package net.fabricmc.example;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.SwordItem;
+import net.minecraft.item.ToolMaterials;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class ExampleMod implements ModInitializer {
+public class SmaragdschwertMod implements ModInitializer {
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
@@ -15,7 +22,8 @@ public class ExampleMod implements ModInitializer {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
-
+		Item Smaragdschwert= new SwordItem(ToolMaterials.DIAMOND,3,-2.4f, new FabricItemSettings().group(ItemGroup.COMBAT));
+		net.minecraft.util.registry.Registry.register(Registry.ITEM,new Identifier("modid","smaragdschwert"),Smaragdschwert);
 		LOGGER.info("Hello Fabric world!");
 	}
 }
--- src/main/java/net/fabricmc/example/mixin/ExampleMixin.java
+++ src/main/java/net/fabricmc/example/mixin/ExampleMixin.java
@@ -1,6 +1,7 @@
 package net.fabricmc.example.mixin;
 
-import net.fabricmc.example.ExampleMod;
+
+import net.fabricmc.example.SmaragdschwertMod;
 import net.minecraft.client.gui.screen.TitleScreen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -11,6 +12,6 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public class ExampleMixin {
 	@Inject(at = @At("HEAD"), method = "init()V")
 	private void init(CallbackInfo info) {
-		ExampleMod.LOGGER.info("This line is printed by an example mod mixin!");
+		SmaragdschwertMod.LOGGER.info("This line is printed by an example mod mixin!");
 	}
 }
